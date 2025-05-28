d7bb693f075515111a95f10dd2dd5a0f7ba11160
BD157 <brindadeep7@gmail.com>
2025-05-28 19:42:00 -0400

    Update utils.py

--- genome-cluster-api/app/utils.py
+++ genome-cluster-api/app/utils.py
@@ -1,11 +1,1 @@
-def generate_kmers(sequence, k=6):
-    return [sequence[i:i+k] for i in range(len(sequence) - k + 1)]
 
-def process_sequence(sequence):
-    # Dummy placeholder logic for clustering
-    kmers = generate_kmers(sequence)
-    return hash(sequence) % 5  # Fake cluster ID for demo
-
-def classify_sequence(sequence):
-    # Dummy placeholder logic for classification
-    return "BA.2" if "A" in sequence else "Other"
